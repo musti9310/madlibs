d57825de8f4cf3489a2463b4c5e61730d39401c7
Mustafa <musti9310@hotmail.com>
2020-06-23 22:19:31 +0300

    new code madlibs

--- madlibs.py
+++ madlibs.py
@@ -1,6 +1,18 @@
 def main():
-	# write your code here
+	print ("Mad libs where libs get mad.")
+	print ("Start below:")
 
+	time=input("Enter a number from 1 to 12:")
+	items=input("Enter a noun (plural):")
+	name=input("Enter a name:")
+	scream=input("Enter any sentence:")
+	action=input("Enter a verb:")
+
+	print("The story goes...")
+	print("It was %s o'clock when I heard a knock at the door." %(time))
+	print("I opened the door and there was a box full of %s with a note saying \"From Mr. %s. \""  %(items, name.title()))
+	print('Just as I closed the door I heard a scream "%s."' %(scream.upper()))
+	print("I froze in place and all I could do was %s." %(action))
 
 
 if __name__ == '__main__':
